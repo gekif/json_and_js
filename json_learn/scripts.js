5a4c7d5d1224861a120b73362b8e766a550f70d9
Gekif <dzulfikar.maulana@gmail.com>
2020-05-16 12:33:06 +0700

    2. Let's start learning JSON: 9. AJAX and JSON part 2

--- json_learn/scripts.js
+++ json_learn/scripts.js
@@ -11,6 +11,19 @@ r.open('GET', 'data.json');
 
 r.onreadystatechange = function () {
     if (r.status === 200 && r.readyState === 4) {
+        var output = '';
 
+        for (var i = 0; i <= data.courses.length; i++) {
+            for (key in data.courses[i]) {
+                if (data.courses[i].hasOwnProperty(key)) {
+                    output += '<li>' + '<a href="' + data.courses[key] + '">' + key + '</a></li>';
+                }
+            }
+        }
     }
-};
+}; // ready state event handler
+
+var update = document.getElementById('social');
+
+update.innerHTML = output.toUpperCase(update);
+
